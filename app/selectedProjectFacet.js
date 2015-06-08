77d3de342e4d64677c70a8ffa32cec1606dbc0b4
JamesUlph <james.ulph@btinternet.com>
2015-06-08 16:29:49 +0100

    Using gen-uid to create unique IDs

--- app/selectedProjectFacet.js
+++ app/selectedProjectFacet.js
@@ -5,12 +5,13 @@ cursors:{
 	 get(state){
 
 	 	if (state.projects.length==0) {
+	 		console.log('facet get 0 length');
 	 		return null;
 	 	};
 	 	if (state.id==null) return null;
 
 
-	 	
+	 	console.log('facet get title');
 
 	 	
 	 	return state.projects[state.id].title;
